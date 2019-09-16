3811a919d3e97a3290504061ca9589c16fb9bdfe
Pijus V <Epochi@users.noreply.github.com>
2019-09-16 08:47:27 +0300

    slick_gallery: iniital commit

---
+++ slick/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.slick-gallery').slick({
+	// lazyLoad: 'ondemand',
+	arrows: true,
+	dots: true,
+	// infinite: true,
+  	// speed: 300,
+  	// slidesToShow: 1,
+  	// adaptiveHeight: true
+  });
+});
